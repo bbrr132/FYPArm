668fab06400dbbef05949ecc9dfe700bc5f33e4c
BR <simbingrui@gmail.com>
2021-09-06 23:18:54 +0800

    Updating IK

--- URDF-Model+IK/IK_Tes.py
+++ URDF-Model+IK/IK_Tes.py
@@ -12,62 +12,67 @@ numJoints = p.getNumJoints(arm)
 for i in range(numJoints):
     print(p.getJointInfo(arm,i))
     
-    
-angle1, angle2, angle3, angle4, angle5,angle6,=p.calculateInverseKinematics(arm,5,coordinate)
+angles=[]
+
+angles=p.calculateInverseKinematics(arm,5,coordinate)
+print("Angles:")
+print(angles)
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=0,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle1,
-                        force=20)
+                        targetPosition=angles[0],
+                        force=200)
 
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=1,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle2,
-                        force=20)
+                        targetPosition=angles[1],
+                        force=200)
 
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=2,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle3,
-                        force=20)
+                        targetPosition=angles[2],
+                        force=200)
 
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=3,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle4,
-                        force=20)
+                        targetPosition=angles[3],
+                        force=200)
 
 
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=4,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle5,
-                        force=20)
+                        targetPosition=angles[4],
+                        force=200)
 
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=5,
                         controlMode=p.POSITION_CONTROL,
-                        targetPosition=angle6,
-                        force=20)
+                        targetPosition=angles[5],
+                        force=200)
 
 
-print(angle1, angle2, angle3, angle4, angle5,angle6)
+
 previoustime=0
 interval=0.1
 previoustime2=0
-interval2=0.5
+interval2=2
 while True:
     
     if (time.time()>previoustime+interval):
         p.stepSimulation()
         previoustime=time.time()
-        print(p.getLinkState(arm,5,computeForwardKinematics=1))
+        #print(p.getLinkState(arm,5,computeForwardKinematics=1))
         
 
     if(time.time()>previoustime2+interval2): 
          previoustime2=time.time()       
+         #coordinate=str(esp.readline().strip().decode("utf-8")).split(',')
          #print(coordinate)
+         print(p.getLinkState(arm,5,computeForwardKinematics=1))
 
     
     
